1fc09c8c0c28cfee5df8712fe2be21806a49708f
Bjarke Sørensen <bs@wasd.dk>
2016-10-24 00:15:19 +0200

    Nu kan du gætte til du keder dig. Og vinde.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,35 @@
+extern crate rand;
+
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+use std::io::Write;
 
 fn main() {
     println!("Gæt tallet!");
-    print!("Skriv dit gæt: ");
 
-    let mut guess = String::new();
+    let secret = rand::thread_rng().gen_range(1, 101);
 
-    io::stdin().read_line(&mut guess)
-        .expect("Kunne ikke indlæse.");
+    println!("Det, ikke særligt, hemmelige nummer er: {}", secret);
 
-    println!("Du gættede på {}", guess);
+    loop {
+        print!("Skriv dit gæt: ");
+        // Hvis du bruger print! skal skærmen manuelt flushes:
+        io::stdout().flush().unwrap();
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Kunne ikke indlæse.");
+
+        let guess: u32 = guess.trim().parse()
+            .expect("Skriv et tal, tak.");
+
+        println!("Du gættede på {}", guess);
+
+        match guess.cmp(&secret) {
+            Ordering::Less    => println!("For lavt."),
+            Ordering::Greater => println!("For højt."),
+            Ordering::Equal   => println!("Du vandt!"),
+        }
+    }
 }
